316ada501932700a56f3a49065963a2c33725723
Josefinx <josefin.wetterberg@gmail.com>
2025-10-21 15:01:29 +0200

    feat: add Tailwind CSS integration and new UI components
    - Updated package.json to include Tailwind CSS, Reka UI, and other dependencies.
    - Created a new tailwind.css file for Tailwind CSS styles and custom theming.
    - Implemented a Button component using shadCN-vue
    - Introduced utility functions for class name merging using clsx and tailwind-merge.
    - Added SSR width plugin for responsive design support.
    - Configured components.json for Shadcn Vue with Tailwind CSS and Lucide icons.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,26 @@
+import tailwindcss from '@tailwindcss/vite'
+
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-10-21",
-  modules: ["@nuxtjs/supabase"],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: ["@nuxtjs/supabase", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./app/components/ui"
+     */
+    componentDir: './app/components/ui'
+  },
   supabase: {
     types: false,
   },
