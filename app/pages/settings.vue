f3dd80d37dd3410534b76e5606d2a39ac0704350
PU-MEriksson <malin.eriksson83@icloud.com>
2025-10-21 12:58:29 +0200

    Add settings page

---
+++ app/pages/settings.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="settings-page">
+    <h1>Settings Page</h1>
+    <p>This is where users can adjust their settings.</p>
+  </div>
+</template>
