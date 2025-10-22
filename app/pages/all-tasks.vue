e313a507b1c18a3e4f90e3def6e7bebb81c50f22
PU-MEriksson <malin.eriksson83@icloud.com>
2025-10-22 09:24:18 +0200

    Add all tasks page

---
+++ app/pages/all-tasks.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>All tasks</h1>
+    <p>Links:</p>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/settings">Settings</NuxtLink>
+    <NuxtLink to="/today">Today</NuxtLink>
+    <NuxtLink to="/all-tasks">All Tasks</NuxtLink>
+    <NuxtLink to="/login">Login</NuxtLink>
+  </div>
+</template>
