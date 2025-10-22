f24b7bfae801121c06702de0de9509b12ad2152c
PU-MEriksson <malin.eriksson83@icloud.com>
2025-10-22 09:25:26 +0200

    Add links to index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1>Välkommen till SupportiveToDo</h1>
-    <p>Din huvudsida är här!</p>
-    <NuxtLink to="/about">Gå till About-sidan</NuxtLink>
+    <h1>Welcome to SupportiveToDo</h1>
+    <p>Here are some links!</p>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/settings">Settings</NuxtLink>
+    <NuxtLink to="/today">Today</NuxtLink>
+    <NuxtLink to="/all-tasks">All Tasks</NuxtLink>
+    <NuxtLink to="/login">Login</NuxtLink>
   </div>
 </template>
