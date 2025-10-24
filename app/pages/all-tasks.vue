492eb8743483de4e2dd67c20d3daab723f04d389
PU-MEriksson <malin.eriksson83@icloud.com>
2025-10-24 12:24:20 +0200

    feat(AllTasks): update task display to include descriptions and simplify task retrieval logic

--- app/pages/all-tasks.vue
+++ app/pages/all-tasks.vue
@@ -1,30 +1,19 @@
 <script setup lang="ts">
-import { useReadTasks } from "~/composables/useReadTasks";
-
-const supabase = useSupabaseClient();
-const user = useSupabaseUser();
-const { tasks, loading, error, getTasks } = useReadTasks();
-
-onMounted(async () => {
-  const {
-    data: { user: u },
-  } = await supabase.auth.getUser();
-
-  if (u?.id) {
-    await getTasks();
-  }
-});
+const { tasks, loading, error, refresh } = useReadTasks();
 </script>
 
 <template>
   <div>
     <h1>All tasks</h1>
+
     <pre v-if="error">{{ error }}</pre>
+
     <div v-else-if="loading">Loading...</div>
+
     <div v-else>
       <ul v-if="tasks.length > 0">
         <li v-for="t in tasks" :key="t.id">
-          {{ t.title }}
+          {{ t.title }} - {{ t.description }}
         </li>
       </ul>
       <p v-else>No tasks found</p>
