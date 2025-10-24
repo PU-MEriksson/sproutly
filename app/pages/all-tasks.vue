94152a4a3491dde2804329c260e850d93a230e10
PU-MEriksson <malin.eriksson83@icloud.com>
2025-10-24 13:25:20 +0200

    feat(AllTasks): add task accordion to all-tasks page

--- app/pages/all-tasks.vue
+++ app/pages/all-tasks.vue
@@ -6,17 +6,12 @@ const { tasks, loading, error, refresh } = useReadTasks();
   <div>
     <h1>All tasks</h1>
 
-    <pre v-if="error">{{ error }}</pre>
-
-    <div v-else-if="loading">Loading...</div>
+    <p v-if="loading">Loading…</p>
+    <p v-else-if="error">{{ error }}</p>
 
     <div v-else>
-      <ul v-if="tasks.length > 0">
-        <li v-for="t in tasks" :key="t.id">
-          {{ t.title }} - {{ t.description }}
-        </li>
-      </ul>
-      <p v-else>No tasks found</p>
+      <TaskAccordion v-for="t in tasks" :key="t.id" :task="t" />
+      <p v-if="tasks.length === 0">No tasks found</p>
     </div>
   </div>
 </template>
