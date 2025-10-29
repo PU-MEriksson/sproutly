bfcff9c98cdcd9318b5938650f075f2cf1e8e50f
PU-MEriksson <malin.eriksson83@icloud.com>
2025-10-29 16:00:04 +0100

    feat: implement ViewTasks component for task display in all-tasks and today pages

--- app/pages/all-tasks.vue
+++ app/pages/all-tasks.vue
@@ -4,14 +4,9 @@ const { tasks, loading, error, refresh } = useTasks();
 
 <template>
   <div>
-    <h1>All tasks</h1>
-
-    <p v-if="loading">Loading…</p>
-    <p v-else-if="error">{{ error }}</p>
-
-    <div v-else>
-      <TaskAccordion v-for="t in tasks" :key="t.id" :task="t" />
-      <p v-if="tasks.length === 0">No tasks found</p>
-    </div>
+    <ViewTasks
+      card-title="All Tasks"
+      card-description="Here are all your tasks."
+    />
   </div>
 </template>
