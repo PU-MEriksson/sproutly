41af35df8c7751d8250b9f6ab196b561646084da
PU-MEriksson <malin.eriksson83@icloud.com>
2025-11-05 10:14:24 +0100

    refactor: wrap template sections in ClientOnly component for improved rendering and to fux hydration errors

--- app/components/ViewTasks.vue
+++ app/components/ViewTasks.vue
@@ -11,18 +11,20 @@ const handleTaskCompleted = (taskTitle: string) => {
 </script>
 
 <template>
-  <section class="bg-neutral-100 p-4">
-    <p v-if="loading">Loading…</p>
-    <p v-else-if="error">{{ error }}</p>
-    <p v-else-if="tasks.length === 0">No tasks found</p>
+  <ClientOnly fallback-tag="section" fallback="">
+    <section class="bg-neutral-100 p-4">
+      <p v-if="loading">Loading…</p>
+      <p v-else-if="error">{{ error }}</p>
+      <p v-else-if="tasks.length === 0">No tasks found</p>
 
-    <div v-else class="space-y-2">
-      <TaskAccordion
-        v-for="t in tasks"
-        :key="t.id"
-        :task="t"
-        @task-completed="handleTaskCompleted"
-      />
-    </div>
-  </section>
+      <div v-else class="space-y-2">
+        <TaskAccordion
+          v-for="t in tasks"
+          :key="t.id"
+          :task="t"
+          @task-completed="handleTaskCompleted"
+        />
+      </div>
+    </section>
+  </ClientOnly>
 </template>
--- app/components/ViewTodaysTasks.vue
+++ app/components/ViewTodaysTasks.vue
@@ -11,18 +11,20 @@ const handleTaskCompleted = (taskTitle: string) => {
 </script>
 
 <template>
-  <section class="bg-neutral-100 p-4">
-    <p v-if="loadingToday">Loading…</p>
-    <p v-else-if="errorToday">{{ errorToday }}</p>
-    <p v-else-if="todaysTasks.length === 0">No tasks found</p>
+  <ClientOnly fallback-tag="section" fallback="">
+    <section class="bg-neutral-100 p-4">
+      <p v-if="loadingToday">Loading…</p>
+      <p v-else-if="errorToday">{{ errorToday }}</p>
+      <p v-else-if="todaysTasks.length === 0">No tasks found</p>
 
-    <div v-else class="space-y-2">
-      <TaskAccordion
-        v-for="t in todaysTasks"
-        :key="t.id"
-        :task="t"
-        @task-completed="handleTaskCompleted"
-      />
-    </div>
-  </section>
+      <div v-else class="space-y-2">
+        <TaskAccordion
+          v-for="t in todaysTasks"
+          :key="t.id"
+          :task="t"
+          @task-completed="handleTaskCompleted"
+        />
+      </div>
+    </section>
+  </ClientOnly>
 </template>
